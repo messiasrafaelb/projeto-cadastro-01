a857df8499932e2a1e217ea9e834e99a7ec56f0a
Rafael <rafaelbernardes201@gmail.com>
2025-07-23 08:29:50 -0300

    Enviando repositório para o github

---
+++ src/test/java/br/com/rafael/projeto_cadastro_01/ProjetoCadastro01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.projeto_cadastro_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCadastro01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
